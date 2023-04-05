3a81328ea95851289403f016e712f273326a4b8c
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-04-05 16:04:42 +0100

    Initial layout

---
+++ main.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+
+/* 3 day forecast for glasgow
+fetch(' http://api.weatherapi.com/v1/forecast.json?key=0cf0102eee904623b4375635230504&q=glasgow&days=3', {mode: 'cors'})
+.then(resp => {
+    return resp.json();
+})
+.then(resp => {
+    console.log(resp);
+});
+*/
