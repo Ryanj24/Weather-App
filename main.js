98f7c88dcbc5a15a3fe7a8a418bb7a6d309e98da
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-04-06 16:25:16 +0100

    Added logic for current day hourly forecast

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const getData = async () => {
 }
 
 getData().then(resp => {
-    console.log(resp);
+    //console.log(resp);
     currentCondition.textContent = resp.current.condition.text;
     currentWind.textContent = `${resp.current.gust_mph}mph`;
     currentRain.textContent = `${resp.current.precip_mm}mm`;
@@ -46,3 +46,66 @@ getData().then(resp => {
     currentCity.textContent = `${resp.location.name}, ${resp.location.country}`;
     currentDateTime.textContent = `${resp.location.localtime}`;
 });
+
+const forecast = async () => {
+    const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=0cf0102eee904623b4375635230504&q=glasgow&days=1&aqi=no&alerts=no', {mode: 'cors'})
+
+    const data = await response.json();
+    return data
+}
+
+forecast().then(resp => {
+    const hoursArr = resp.forecast.forecastday[0].hour;
+
+    hoursArr.forEach(hr => {
+        createHourlyTab(hr);
+    })
+    /* Get time for each hour 
+    console.log(hoursArr.chance_of_rain);*/
+})
+
+
+function createHourlyTab(obj) {
+
+    const todaysWeatherDiv = document.querySelector('.todays-weather');
+
+    const container = document.createElement('div');
+    container.classList.add('hour-forecast');
+
+    const hourDiv = document.createElement('div');
+    hourDiv.classList.add('hour');
+    const timeHeader = document.createElement('h1');
+    timeHeader.textContent = `${obj.time.slice(11)}`;
+    hourDiv.appendChild(timeHeader);
+    container.appendChild(hourDiv);
+
+    const condition = document.createElement('div');
+    condition.classList.add('condition');
+    const conditionText = document.createElement('p');
+    conditionText.textContent = `${obj.condition.text}`;
+    const conditionImg = document.createElement('img');
+    conditionImg.src = `./assets${obj.condition.icon.slice(34)}`;
+    condition.appendChild(conditionText);
+    condition.appendChild(conditionImg);
+    container.appendChild(condition);
+
+    const tempRain = document.createElement('div');
+    tempRain.classList.add('highs-lows');
+
+    const temp = document.createElement('div');
+    temp.classList.add('high-temp');
+    const tempNum = document.createElement('p');
+    tempNum.textContent = `Temperature: ${Math.round(obj.temp_c)}\xB0C`;
+    temp.appendChild(tempNum);
+    tempRain.appendChild(temp);
+
+    const rainChance = document.createElement('div');
+    rainChance.classList.add('rain-chance');
+    const rain = document.createElement('p');
+    rain.textContent = `Rain chance: ${obj.chance_of_rain}%`;
+    rainChance.appendChild(rain);
+    tempRain.appendChild(rainChance);
+    container.appendChild(tempRain);
+
+    todaysWeatherDiv.appendChild(container);
+}
