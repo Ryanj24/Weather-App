4a1b254953f00a41f063f6b93183e59601ca4605
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-04-06 09:46:03 +0100

    Added initial logic

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const container = document.querySelector('.container');
+const currentCondition = document.getElementById('condition-text');
+const currentWind = document.getElementById('current-wind');
+const currentRain = document.getElementById('current-rain');
+const currentHumidity = document.getElementById('current-humidity');
+const currentTemp = document.getElementById('current-temperature');
+
+const currentCity = document.getElementById('city');
+const currentDateTime = document.getElementById('date-time');
+
 
 /* 3 day forecast for glasgow
 fetch(' http://api.weatherapi.com/v1/current.json?key=0cf0102eee904623b4375635230504&q=glasgow&days=3', {mode: 'cors'})
@@ -17,3 +26,23 @@ fetch('http://api.weatherapi.com/v1/forecast.json?key=0cf0102eee904623b437563523
 .then(resp => {
     console.log(resp.forecast.forecastday[0].hour);
 });*/
+
+
+const getData = async () => {
+    const response = await fetch(' http://api.weatherapi.com/v1/current.json?key=0cf0102eee904623b4375635230504&q=glasgow&days=3', {mode: 'cors'});
+
+    const data = await response.json();
+    return data
+}
+
+getData().then(resp => {
+    console.log(resp);
+    currentCondition.textContent = resp.current.condition.text;
+    currentWind.textContent = `${resp.current.gust_mph}mph`;
+    currentRain.textContent = `${resp.current.precip_mm}mm`;
+    currentHumidity.textContent = `${resp.current.humidity}%`;
+    currentTemp.textContent = `${resp.current.temp_c}\xB0C`;
+
+    currentCity.textContent = `${resp.location.name}, ${resp.location.country}`;
+    currentDateTime.textContent = `${resp.location.localtime}`;
+});
