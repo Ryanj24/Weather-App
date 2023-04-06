410a31c43b4dfd0bd917192346fedc1aa9480b8e
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-04-06 09:25:55 +0100

    Added more styling

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const container = document.querySelector('.container');
 
 /* 3 day forecast for glasgow
-fetch(' http://api.weatherapi.com/v1/forecast.json?key=0cf0102eee904623b4375635230504&q=glasgow&days=3', {mode: 'cors'})
+fetch(' http://api.weatherapi.com/v1/current.json?key=0cf0102eee904623b4375635230504&q=glasgow&days=3', {mode: 'cors'})
 .then(resp => {
     return resp.json();
 })
 .then(resp => {
     console.log(resp);
-});
-*/
+});*/
+
+/* forecast each hour of 24hr period
+fetch('http://api.weatherapi.com/v1/forecast.json?key=0cf0102eee904623b4375635230504&q=glasgow&days=1&aqi=no&alerts=no', {mode: 'cors'})
+.then(resp => {
+    return resp.json();
+})
+.then(resp => {
+    console.log(resp.forecast.forecastday[0].hour);
+});*/
